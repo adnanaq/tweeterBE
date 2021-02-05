86de3e126c4d5c8c6a3fb087e5efcebcc00f87a4
Dani <adnan_aq@hotmail.com>
2021-02-04 18:50:31 -0600

    Install the express-validator package to validate the user login and signup information. Insert the code logic into both files, loginRoutes and signupRoute. Also, install the jwt package for the session handlign.

--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -1,9 +1,73 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { validationResult, check } = require("express-validator/check");
+
 const app = express();
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  res.json({ message: "This is login route!" });
-});
+const User = require("../schema/user");
+
+router.post(
+  "/",
+  [
+    check("email", "Enter Valid Email!").isEmail(),
+    check("password", "Enter Valid Password").isLength({
+      min: 8,
+    }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array(),
+      });
+    }
+
+    const { email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+
+      if (!user) {
+        return res.status(400).json({
+          message: "User Does Not Exist!",
+        });
+      }
+
+      const passwordMatch = await bcrypt.compare(password, user.password);
+
+      if (!passwordMatch) {
+        return res.status(400).json({
+          message: "Incorrect Password! Enter the correct Password!",
+        });
+      }
+
+      const payload = {
+        user: { id: user.id },
+      };
+
+      jwt.sign(
+        payload,
+        "randomString",
+        {
+          expiresIn: 3600,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.status(200).json({
+            token,
+          });
+        }
+      );
+    } catch (e) {
+      console.error(e);
+      res.status(500).json({
+        message: "Server Error",
+      });
+    }
+    // res.json({ message: "This is login route!" });
+  }
+);
 
 module.exports = router;
--- routes/signupRoutes.js
+++ routes/signupRoutes.js
@@ -1,19 +1,70 @@
 const express = require("express");
-const bodyParser = require("body-parser");
+// const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { validationResult, check } = require("express-validator/check");
 
 const app = express();
 const router = express.Router();
+
 const User = require("../schema/user");
 
-app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.urlencoded({ extended: false }));
 
-router.get("/", (req, res, next) => {
-  res.status(200).json({ message: "This is register route!" });
-});
+/**
+ * @method - POST
+ * @param - /signup
+ * @description - User signup and validation
+ */
 
-router.post("/", (req, res, next) => {
-  res.status(200).json({ message: "This is register route!" });
-});
+router.post(
+  "/",
+  [
+    check("username", "Enter Valid Username!").not().isEmpty(),
+    check("email", "Enter Valid Email!").isEmail(),
+    check("password", "Enter Valid Password").isLength({ min: 8 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    console.log(req.body);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ err: errors.array(), msg: "confirmed" });
+    }
+
+    const { username, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+      if (user) {
+        return res.status(400).json({
+          message: "User Already Exist",
+        });
+      }
+
+      user = new User({
+        username,
+        email,
+        password,
+      });
+
+      console.log(this.user);
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      const payload = { user: { id: user.id } };
+
+      jwt.sign(payload, "randomString", { expiresIn: 10000 }, (err, token) => {
+        if (err) throw err;
+        res.status(200).json({ token });
+      });
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send("Could not save the User!");
+    }
+  }
+);
 
 module.exports = router;
